d12326f46486fb89bcbba10e93065f71aeded00f
ShivamHP <shivampachchigar14112@gmail.com>
2023-02-15 14:54:00 +0530

    Initial commit
    The initial commit includes the following:
    - abha-services microservice: handles all the API calls to ABDM.
    - user-services microservice: handles CRUD operations for users.
    - parchaa-ndhm gateway: a single gateway through which all the APIs are accessible

---
+++ apps/user-services/src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { UserServicesModule } from './user-services.module';
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    UserServicesModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: 8082,
+        retryAttempts: 3,
+        retryDelay: 1000
+      }
+    }
+  )
+  await app.listen()
+  console.log('user-services started')
+}
+bootstrap();
